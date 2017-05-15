3bcbad848151d2109887cf963a7354fee22015aa
zhangyingwei <zhangyw001@gmail.com>
2017-05-15 22:00:43 +0800

    网上找了一个表格，看起来还行，搞了一下，试试吧，很可能就这个了

--- src/main/resources/static/js/admin/article/history-index.js
+++ src/main/resources/static/js/admin/article/history-index.js
@@ -1,9 +1,51 @@
 $(function () {
+    var data = [];
+    for (var i = 0;i<100;i++){
+        data.push({
+            id:i,
+            title:'title',
+            subpath:'123'
+        })
+    }
     new FancyGrid({
         renderTo: 'article_container',
-        data: [],
+        title: ' ',
+        data: data,
+        paging: true,
+        paging: {
+            pageSize: 20,
+            refreshButton:true
+        },
+        height:700,
+        selModel: 'rows',
+        trackOver: true,
+        defaults: {
+            type: 'string',
+            width: 100,
+            editable: true,
+            sortable: true,
+            resizable: true
+        },
+        tbar: [{
+            type: 'button',
+            text: '增加一行',
+            width:100,
+            action: 'add'
+        },{
+            type: 'button',
+            text: '编辑',
+            width:100,
+            action: 'edit'
+        },{
+            type: 'button',
+            text: '删除',
+            width:100,
+            action: 'remove'
+        }],
         columns: [{
             index: 'id',
+            locked: true,
+            editable: false,
             title: '编号',
             type: 'string',
             width: 100
@@ -11,7 +53,11 @@ $(function () {
             index: 'title',
             title: '标题',
             type: 'string',
-            width: 200
+            width: 200,
+            filter: {
+                header: true,
+                emptyText: '查询'
+            }
         },{
             index: 'subpath',
             title: '自定义访问路径',
