ec919f3b256b697cee7e9a401aaf5885939914e7
zhangyingwei <zhangyw001@gmail.com>
2017-05-23 22:23:06 +0800

    觉得之前找的那个表格有点丑，并且也不是很好用，后来想着还是用element的表格吧，毕竟美啊，但是jquery + vue混胡感觉有点不伦不类

--- src/main/resources/static/js/admin/article/history-index.js
+++ src/main/resources/static/js/admin/article/history-index.js
@@ -1,88 +1,24 @@
 $(function () {
-    var data = [];
-    for (var i = 0;i<100;i++){
-        data.push({
-            id:i,
-            title:'title',
-            subpath:'123'
-        })
-    }
-    new FancyGrid({
-        renderTo: 'article_container',
-        title: ' ',
-        data: data,
-        paging: true,
-        paging: {
-            pageSize: 20,
-            refreshButton:true
-        },
-        height:700,
-        selModel: 'rows',
-        trackOver: true,
-        defaults: {
-            type: 'string',
-            width: 100,
-            editable: true,
-            sortable: true,
-            resizable: true
-        },
-        tbar: [{
-            type: 'button',
-            text: '增加一行',
-            width:100,
-            action: 'add'
-        },{
-            type: 'button',
-            text: '编辑',
-            width:100,
-            action: 'edit'
-        },{
-            type: 'button',
-            text: '删除',
-            width:100,
-            action: 'remove'
-        }],
-        columns: [{
-            index: 'id',
-            locked: true,
-            editable: false,
-            title: '编号',
-            type: 'string',
-            width: 100
-        },{
-            index: 'title',
-            title: '标题',
-            type: 'string',
-            width: 200,
-            filter: {
-                header: true,
-                emptyText: '查询'
-            }
-        },{
-            index: 'subpath',
-            title: '自定义访问路径',
-            type: 'string',
-            width: 200
-        },{
-            index: 'intro',
-            title: '简介',
-            type: 'string',
-            width: 300
-        },{
-            index: 'kind',
-            title: '类别',
-            type: 'string',
-            width: 100
-        },{
-            index: 'usecommont',
-            title: '允许评论',
-            type: 'string',
-            width: 100
-        },{
-            index: 'flag',
-            title: '状态',
-            type: 'string',
-            width: 100
-        }]
-    });
+    new Vue({
+        el: "#article_container",
+        data: {
+            tableData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1517 弄'
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1519 弄'
+            }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1516 弄'
+            }]
+        }
+    })
 })
