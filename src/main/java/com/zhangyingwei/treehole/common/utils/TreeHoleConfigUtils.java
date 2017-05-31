770bb4d6f6b020fdae5c803a662811d8933a2b23
zhangyw <zhangyw001@gmail.com>
2017-05-31 18:07:45 +0800

    新建里TreeholeConfigUtils类来统一处理配置信息

---
+++ src/main/java/com/zhangyingwei/treehole/common/utils/TreeHoleConfigUtils.java
@@ -0,0 +1,13 @@
+package com.zhangyingwei.treehole.common.utils;
+
+import java.util.Map;
+
+/**
+ * Created by zhangyw on 2017/5/31.
+ */
+public class TreeHoleConfigUtils {
+
+    public static Map<String,Object> readThremeYmlConfig(){
+        return null;
+    }
+}
